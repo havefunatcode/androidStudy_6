7c02e44ea0bced81fe31038b44e9e1a96313b389
TaeDo <dlxogh439@gmail.com>
2020-10-27 08:58:02 +0900

    Add My Application2

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClicked(View v)   {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-4242-7531"));
+        startActivity(intent);
+    }
+}
